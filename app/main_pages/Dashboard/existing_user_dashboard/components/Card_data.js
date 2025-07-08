582f1c5e5744fbe9006b67571162b5211193fd50
HeywhyREX <oyeniyan2000@gmail.com>
2025-07-08 03:49:22 +0100

    on it

--- app/main_pages/Dashboard/existing_user_dashboard/components/Card_data.js
+++ app/main_pages/Dashboard/existing_user_dashboard/components/Card_data.js
@@ -2,29 +2,29 @@ export const Card_data=[
     {
         id:1,
         card_title:'Total Revenue',
-        cardSum:'UGX810.00',
-        percentageIncrease:'10.6%',
+        cardSum:'UGX0',
+        percentageIncrease:'0%',
         status:'increase'
     },
     {
         id:2,
         card_title:'Total Customer',
-        cardSum:'56.00',
-        percentageIncrease:'1.5%',
+        cardSum:'0',
+        percentageIncrease:'0%',
          status:'increase'
     },
     {
         id:3,
         card_title:'Total Transactions',
-        cardSum:'200.00',
-        percentageIncrease:'3.6%',
+        cardSum:'0',
+        percentageIncrease:'0%',
          status:'increase'
     },
     {
         id:4,
         card_title:'Total Product',
-        cardSum:'551.00',
-        percentageIncrease:'1.5%',
+        cardSum:'0',
+        percentageIncrease:'0%',
          status:'decrease'
     },
 ]
